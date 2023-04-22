2823c369d6e9fc215003a88a618906255aedc679
PatrickTali <ptaliaferro218@gmail.com>
2023-04-22 02:17:45 -0400

    'initial'

---
+++ car_inventory/models.py
@@ -0,0 +1,93 @@
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+from datetime import datetime
+
+#adding flask security for passwords
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#import secrets module (from python) generates a token for each user
+import secrets
+
+#import flask login to check for an authenticated user and store current user
+from flask_login import UserMixin, LoginManager
+
+#import flask marshmallow to help create our Schemas 
+from flask_marshmallow import Marshmallow 
+
+db = SQLAlchemy() 
+login_manager = LoginManager()
+ma = Marshmallow()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.String, primary_key = True)
+    first_name = db.Column(db.String(150), nullable = True, default = '')
+    last_name = db.Column(db.String(150), nullable = True, default = '')
+    email = db.Column(db.String(150), nullable = False)
+    password = db.Column(db.String(150), nullable = True, default = '')
+    username = db.Column(db.String(150), nullable = False)
+    token = db.Column(db.String, default = '', unique = True)
+    date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    
+
+    def __init__(self, email, username, first_name = '', last_name = '', password = ''):
+        self.id = self.set_id()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = self.set_password(password)
+        self.email = email
+        self.username = username
+        self.token = self.set_token(24)
+
+    def set_token(self, length):
+        return secrets.token_hex(length)
+    
+    def set_id(self):
+        return str(uuid.uuid4())
+    
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+        return self.pw_hash
+    
+    def __repr__(self):
+        return f"User {self.email} has been added to the database!"
+    
+
+class Car(db.Model):
+    car_id = db.Column(db.String, primary_key = True)
+    make= db.Column(db.String(25), nullable=True)
+    price = db.Column(db.Numeric(precision=10, scale=2))
+    model = db.Column(db.String(25), nullable=True)
+    year = db.Column(db.Integer, nullable=True)
+    vin = db.Column(db.String(50))
+    customer_id = db.Column(db.Integer)
+   
+
+    def __init__(self, car_id, price, make, model, year, vin, customer_id):
+        self.car_id = self.car_id()
+        self.make = make
+        self.model = model
+        self.price = price
+        self.year = year
+        self.vin = vin
+        self.customer_id = customer_id
+       
+
+    def set_id(self):
+        return str(uuid.uuid4())
+
+    def __repr__(self):
+        return f"Car {self.name} has been added to the database!"
+
+
+
+class CarSchema(ma.Schema): 
+    class Meta:
+        fields = ['id', 'name', 'description', 'price',  
+                  '','series','random_joke']
+
+car_schema = CarSchema()
+car_schema = CarSchema(many = True)
--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+
 
 
 load_dotenv(os.path.join(basedir, '.env'))
@@ -10,12 +14,10 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config():
+    """"""
 
-    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_APP = os.environ.get('Flask_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
-
-
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("Database_URL") or "sqlite:///" + os.path.join(basedir, 'app.db')
-    SQLACHEMY_TRACK_MODIFICATIONS = False
-    
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "nana nana"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  
